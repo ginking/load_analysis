6430f570c0573f75191ade585e4d04bcd592ae01
Mike Metral <mdmetra@sandia.gov>
2012-03-12 11:58:05 -0700

    Init commit of utils.py

---
+++ utils.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import logging
+
+class Utils:
+    def set_logging(self, logging_level):
+        logging.basicConfig(level=logging_level,
+            format='%(asctime)s %(levelname)s: %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S')
+
+    @staticmethod
+    def get_dir_files(directory):
+        logging.debug("Listing directory: %s" % (directory))
+        dirlist = os.listdir(directory)
+        filelist = []
+        for i in dirlist:
+            if os.path.isfile("%s/%s" % (directory,i)):
+                logging.debug("Found file: %s%s", directory, i)
+                filelist.append([directory, i])
+        return filelist
