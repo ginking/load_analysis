1d5bc357bcbc452f6be509c6e786655f8168fc06
Mike Metral <mdmetra@sandia.gov>
2012-03-12 17:42:38 -0700

    Added FileAnalysis & DataResults to encapsulate analysis data

--- objects.py
+++ objects.py
@@ -1,5 +1,14 @@
 class FileData:
-    def __init__(self, file_id, timestamps, deltas):
+    def __init__(self, file_id=None, timestamps=None, deltas=None):
         self.file_id = file_id
         self.timestamps = timestamps
         self.deltas = deltas
+class FileAnalysis:
+    def __init__(self, file_id, mean, std):
+        self.file_id = file_id
+        self.mean = mean
+        self.std = std
+class DataResults:
+    def __init__(self, analysis, mean_of_all_stds):
+        self.analysis = analysis
+        self.mean_of_all_stds = mean_of_all_stds
