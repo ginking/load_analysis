2c50cfb77007791b74e40d60de3b9be11654dda4
Mike Metral <mdmetra@sandia.gov>
2012-03-14 15:42:46 -0700

    Major abstraction of code, log msg editing, added comments & restructured code
    - Removed unused variables
    - Added in more descriptive comments to major code blocks
    - Abstracted reused functions i.e. collect_all_timestamps & collect_all_deltas
    - Moved the logging of trimming errors when a trimmed list is empty to the
      trim_lists_by_common_threshold() as it was more appropriate there than in
      print_file_data()
    - Renamed compute_mean_and_std() back to analyze() to be a more generic name in
      case that more computations or analysis wishes to be done in the future
    - Tweaked log messages
    - Renamed print_file_data() -> log_debug_file_data() as it was more appropriate
      of a name
    - Removed prepare_plot as collect_all_deltas() rendered it useless

--- load_analysis_lib.py
+++ load_analysis_lib.py
@@ -12,117 +12,136 @@ from objects import FileData
 
 class LoadAnalysisLib:
 #-------------------------------------------------------------------------------
-    utils = Utils()
-#-------------------------------------------------------------------------------
     @staticmethod
     def set_logging(logging_level):
+        # Set up the logging configuration and format
+
         logging.basicConfig(level=logging_level,
             format='%(asctime)s %(levelname)s: %(message)s',
             datefmt='%Y-%m-%d %H:%M:%S')
 #-------------------------------------------------------------------------------
     @staticmethod
-    def parse_output_files(args):
+    def parse_data_files(args):
+        # Parse & process the data given
+
         all_file_data = []
         extra_headers_found = False
 
+        # Get the file listing for each directory
         for arg in args:
-            all_files = Utils.get_dir_files(arg)
+            dir_listing = Utils.get_dir_listing(arg)
             logging.debug("-------------------------------------------------")
             logging.debug("All files found:")
             logging.debug("----------------")
-            logging.debug("%s", all_files)
+            logging.debug("%s", dir_listing)
 
-        for file_entry in all_files:
-            file_data = []
+            # Process each file in the directory
+            for file_entry in dir_listing:
+                dirname, filename = file_entry
+                file_input = open("%s/%s" % (dirname, filename), "r")
 
-            dirname, filename = file_entry
+                # Data to collect
+                timestamps = []
+                deltas = []
+                total_headers_found = 0
 
-            file_input = open("%s/%s" % (dirname, filename), "r")
-            timestamps = []
-            deltas = []
+                # Fix the filepath and make that the file_id
+                filepath = Utils.fix_filepath(dirname, filename)
+                file_id = filepath
 
-            extra_headers_found_count = -1
+                # Parses the columns of the file input
+                for column in file_input:
+                    column_data = column.split();
+                    try:
+                        timestamp = int(column_data[0])
+                        delta = int(column_data[1])
+                        
+                        timestamps.append(timestamp)
+                        deltas.append(delta)
+                    except:
+                        total_headers_found += 1
 
-            if not dirname.endswith('/'):
-                file_id = '/'.join([dirname,filename])
-            else:
-                file_id = ''.join([dirname,filename])
+                # Report if extra headers are found aside from the initial one
+                if total_headers_found > 1:
+                    extra_headers_found = True
+                    logging.error(\
+                        "-------------------------------------------------")
+                    logging.error("%s extra header(s) found in file: %s" % \
+                            (total_headers_found-1, file_id))
 
-            for column in file_input:
-                column_data = column.split();
-                try:
-                    timestamp = int(column_data[0])
-                    delta = int(column_data[1])
-                    
-                    timestamps.append(timestamp)
-                    deltas.append(delta)
-                except:
-                    extra_headers_found_count += 1
-                    #continue
+                file_data = FileData(file_id, timestamps, deltas)
+                all_file_data.append(file_data)
 
-            if extra_headers_found_count > 0:
-                extra_headers_found = True
-                logging.error(\
-                    "-------------------------------------------------")
-                logging.error("%s extra header(s) found in file: %s" % \
-                        (extra_headers_found_count, file_id))
-
-            new_file_data = FileData(file_id, timestamps, deltas)
-            all_file_data.append(new_file_data)
-
-        if extra_headers_found:
-            None
-            #sys.exit(0)
+            if extra_headers_found:
+                # Exit as there is an error with the data collectected
+                None
+                #sys.exit(0)
 
         return all_file_data
 
+
 #-------------------------------------------------------------------------------
     @staticmethod
-    def trim_lists_by_common_threshold(all_file_data):
-        # trims each individual list so that they all share timestamps of
-        # equal or greater value relative to the biggest head out of all of the
-        # original lists (aka the least common threshold of all of the lists)
+    def compute_timestamp_threshold(file_data_list):
+        # Computes a timestamp threshold shared amongst the file data, where
+        # *Note*: Requires each set of file data in file_data_list to be
+        # independently sorted in ascending order
 
-        # Requires each individual list in all_file_data to be sorted in
-        # ascending order
-
-        if not all_file_data:
-            return []
-
+        # Init values to store biggest timestamp
         biggest_timestamp = -1
         biggest_timestamp_file_id = None
         
-        # find the biggest timestamp at the head of each list - this will be our
-        # threshold for trimming
         logging.info("-------------------------------------------------")
-        logging.info("Computing timestamp threshold amongst all file data ...")
+        logging.info("Computing timestamp threshold amongst file data ...")
 
-        for file_data in all_file_data:
+        # Find the biggest timestamp at the head of each list - this will be our
+        # threshold
+        for file_data in file_data_list:
             file_id = file_data.file_id
             timestamps = file_data.timestamps
             deltas = file_data.deltas
 
             head_timestamp = timestamps[0]
 
+            logging.debug("")
             logging.debug("viewing head timestamp: " + str(head_timestamp))
+
             if biggest_timestamp < head_timestamp:
                 biggest_timestamp = head_timestamp
                 biggest_timestamp_file_id = file_id
+
             logging.debug("current biggest timestamp: " + \
                     str(biggest_timestamp))
-            logging.debug("")
+        
+        return biggest_timestamp, biggest_timestamp_file_id
 
-        threshold = biggest_timestamp
+#-------------------------------------------------------------------------------
+    @staticmethod
+    def trim_lists_by_common_threshold(all_file_data):
+        # Trims each file's data using a timestamp threshold such that the
+        # trimmed data has an equal or greater value relative to the 
+        # biggest head out of the file data 
+
+        if not all_file_data:
+            return []
+
+        # Compute the timestamp threshold
+        threshold, threshold_file_id = \
+                LoadAnalysisLib.compute_timestamp_threshold(all_file_data)
+
         logging.info("-------------------------------------------------")
         logging.info("Timestamp threshold set at: (%s) by file: (%s)" \
-                %  (str(threshold), biggest_timestamp_file_id))
+                %  (str(threshold), threshold_file_id))
 
-        iterations = 0
+        debug_iterations = 0
 
-        # iterate through each list & trim out timestamp/output entries that do
+        # Track any empty trimmed file data sets as a result of the threshold
+        empty_trimmed_file_data_by_file_data_id = []
+
+        # Iterate through each list & trim out timestamp/delta entries that do
         # not meet the threshold
         for file_data_index, file_data in enumerate(all_file_data):
-            iterations += 1
+            debug_iterations += 1
             trimmed_timestamps = []
             trimmed_deltas = []
 
@@ -131,29 +150,72 @@ class LoadAnalysisLib:
             deltas = file_data.deltas
 
             for timestamp_index, timestamp in enumerate(timestamps):
-                iterations += 1
+                debug_iterations += 1
                 if timestamp >= threshold:
+                    # trim the timestamp & deltas list from this point forward
                     trimmed_timestamps = timestamps[timestamp_index:]
                     trimmed_deltas = deltas[timestamp_index:]
                     break
-            file_data.timestamps = trimmed_timestamps
-            file_data.deltas = trimmed_deltas
-            all_file_data[file_data_index] = file_data
+
+            # If the trimmed file data is empty track it, else store it
+            if len(trimmed_timestamps) == 0 or len(trimmed_deltas) == 0:
+                empty_trimmed_file_data_by_file_data_id.append(file_id)
+            else:
+                file_data.timestamps = trimmed_timestamps
+                file_data.deltas = trimmed_deltas
+                all_file_data[file_data_index] = file_data
 
         logging.debug("-------------------------------------------------")
-        logging.debug("total iterations: " + str(iterations))
+        logging.debug("total debug iterations: " + str(debug_iterations))
+
+        # Report any empty lists and quit
+        if len(empty_trimmed_file_data_by_file_data_id) > 0:
+            logging.error("-----------------------------------------------")
+            logging.error("Trimming the file data by " + \
+                "the timestamp threshold resulted in an empty data set " + \
+                "for files: ")
+
+            for file_id in empty_trimmed_file_data_by_file_data_id:
+                logging.error("File: " + file_id)
+            logging.error("Revise those files, or exclude them to " + \
+                    "continue analyzing the data")
+
+            sys.exit(0)
 
         return all_file_data
 
 #-------------------------------------------------------------------------------
     @staticmethod
-    def compute_median_std(file_data_list, dataset_name, log=False):
+    def collect_all_timestamps(file_data_list):
+        # Collect the timestamps from each file input
+
+        all_timestamps = []
+
+        for file_data in file_data_list:
+            timestamps = file_data.timestamps
+            all_timestamps += timestamps
+
+        return all_timestamps
+#-------------------------------------------------------------------------------
+    @staticmethod
+    def collect_all_deltas(file_data_list):
+        # Collect the deltas from each file input
+
         all_deltas = []
 
-        for file_data_index, file_data in enumerate(file_data_list):
+        for file_data in file_data_list:
             deltas = file_data.deltas
             all_deltas += deltas
 
+        return all_deltas
+#-------------------------------------------------------------------------------
+    @staticmethod
+    def analyze(file_data_list, dataset_name, log=False):
+        # Analyze file data by performing mathematical computations on it
+
+        all_deltas = LoadAnalysisLib.collect_all_deltas(file_data_list)
+
+        # Compute the following for all deltas
         median = numpy.median(all_deltas)
         std = numpy.std(all_deltas)
 
@@ -166,23 +228,24 @@ class LoadAnalysisLib:
 #-------------------------------------------------------------------------------
     @staticmethod
     def cleanup_file_data(all_file_data, cleanup_level):
-        # do cleanup upto removing the +/- level of stds indicated by
-        # clean_std_level
+        # Clean up the file data by removing any data values that are not +/-
+        # the level of stds indicated by cleanup_level
+
         logging.info("-------------------------------------------------")
         logging.info("Cleaning up outliers in the data that " + \
-                "are past +/- (%s) standard deviation(s) ..." % \
+                "are not within +/- (%s) standard deviation(s) ..." % \
                 str(cleanup_level))
 
-        median, std = \
-            LoadAnalysisLib.compute_median_std(all_file_data, "original")
-
+        # Analyze data & set up cleaning boundaries
+        median, std = LoadAnalysisLib.analyze(all_file_data, "original")
         lower_boundary = median - (std * cleanup_level)
         upper_boundary = median + (std * cleanup_level)
 
         all_file_data_cleaned = []
 
+        # Scrub the file data for values that are not within the cleaning
+        # boundaries
         for file_data_index, file_data in enumerate(all_file_data):
-
             clean_file_data = FileData()
             clean_timestamps = []
             clean_deltas = []
@@ -196,28 +259,34 @@ class LoadAnalysisLib:
                     clean_timestamps.append(timestamps[delta_index])
                     clean_deltas.append(deltas[delta_index])
             
+            # If the clean timestamps & deltas are not empty lists, store them
             if clean_timestamps and clean_deltas:
                 clean_file_data.file_id = file_id
                 clean_file_data.timestamps = clean_timestamps
                 clean_file_data.deltas = clean_deltas
                 all_file_data_cleaned.append(clean_file_data)
 
-        for i, f in enumerate(all_file_data_cleaned):
-            timestamps_len = len(f.timestamps)
-            deltas_len = len(f.deltas)
+        # If debug set, print clean up logs
+        logging_level = \
+                logging.getLevelName(logging.getLogger().getEffectiveLevel())
+        if logging_level == 'DEBUG':
+            for clean_file_data in all_file_data_cleaned:
+                timestamps_len = len(clean_file_data.timestamps)
+                deltas_len = len(clean_file_data.deltas)
 
-            logging.debug("cleanup-file: " + f.file_id + \
-            " -- total timestamps: " + str(timestamps_len) + \
-            " -- total deltas: " + str(deltas_len))
+                logging.debug("cleanup-file: " + clean_file_data.file_id + \
+                " -- total timestamps: " + str(timestamps_len) + \
+                " -- total deltas: " + str(deltas_len))
 
         return all_file_data_cleaned
 
 #-------------------------------------------------------------------------------
     @staticmethod
-    def compute_mean_of_all_file_data_stds(trimmed_file_data):
+    def compute_mean_of_file_data_stds(file_data_list):
         all_stds = []
 
-        for index, file_data in enumerate(trimmed_file_data):
+        # Compute the std of the deltas for each file input & collect it
+        for index, file_data in enumerate(file_data_list):
             deltas = file_data.deltas
             std = numpy.std(deltas)
 
@@ -225,20 +294,18 @@ class LoadAnalysisLib:
 
         mean_of_all_stds = numpy.mean(all_stds)
         logging.info("-------------------------------------------------")
-        logging.info("Mean of all standard deviations of the deltas: (%s)" % \
-               str(mean_of_all_stds))
+        logging.info("Mean of all delta standard deviations found " + \
+                "(from all file data): (%s)" % str(mean_of_all_stds))
         
 #-------------------------------------------------------------------------------
     @staticmethod
-    def print_file_data(title, file_data_list):
+    def log_debug_file_data(title, file_data_list):
         # Prints the file data provided
-
-        # only for when printing the trimmed data
-        empty_trimmed_file_data_by_file_data_id = []
 
         logging.debug("-------------------------------------------------")
         logging.debug(title + " data:")
         logging.debug("---------------")
+
         for index, file_data in enumerate(file_data_list):
             timestamps_len = len(file_data.timestamps)
             deltas_len = len(file_data.deltas)
@@ -247,31 +314,4 @@ class LoadAnalysisLib:
             " -- total timestamps: " + str(timestamps_len) + \
             " -- total deltas: " + str(deltas_len))
             
-            if title == "trimmed":
-                if timestamps_len == 0 or deltas_len == 0:
-                    empty_trimmed_file_data_by_file_data_id.append(\
-                        file_data.file_id)
-
-        # Report any empty lists and quit
-        if title == "trimmed":
-            if len(empty_trimmed_file_data_by_file_data_id) > 0:
-                logging.error("-----------------------------------------------")
-                logging.error("Trimming the original file data by " + \
-                    "the timestamp threshold resulted in an empty data set " + \
-                    "for files: ")
-                for file_id in empty_trimmed_file_data_by_file_data_id:
-                    logging.error("File: " + file_id)
-                sys.exit(0)
-
-#-------------------------------------------------------------------------------
-    @staticmethod
-    def prepare_plot_data(all_file_data_trimmed):
-        all_timestamps = []
-        all_deltas = []
-
-        for file_data in all_file_data_trimmed:
-            all_timestamps += file_data.timestamps
-            all_deltas += file_data.deltas
-
-        return all_timestamps, all_deltas
 #-------------------------------------------------------------------------------
