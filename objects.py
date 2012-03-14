928848e6d48686b01f147503b2d51374aa383578
Mike Metral <mdmetra@sandia.gov>
2012-03-14 15:37:56 -0700

    Added comments

--- objects.py
+++ objects.py
@@ -4,6 +4,8 @@
 # Date: 03/14/12
 
 class FileData:
+    # Class to store the data from each file input
+
     def __init__(self, file_id=None, timestamps=None, deltas=None):
         self.file_id = file_id
         self.timestamps = timestamps
