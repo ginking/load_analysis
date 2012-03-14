62b00f947e2cb3d1e45788233debdc72c430a88e
Mike Metral <mdmetra@sandia.gov>
2012-03-13 17:06:21 -0700

    Removed the FileAnalysis & DataResults objects as they were useless

--- objects.py
+++ objects.py
@@ -3,12 +3,3 @@ class FileData:
         self.file_id = file_id
         self.timestamps = timestamps
         self.deltas = deltas
-class FileAnalysis:
-    def __init__(self, file_id, mean, std):
-        self.file_id = file_id
-        self.mean = mean
-        self.std = std
-class DataResults:
-    def __init__(self, analysis, mean_of_all_stds):
-        self.analysis = analysis
-        self.mean_of_all_stds = mean_of_all_stds
